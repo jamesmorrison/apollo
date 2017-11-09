5d7487c71d86062b77c48aac43f0b7313555e1a1
James Morrison <james@morrison.me>
2017-11-09 14:10:38 +0000

    Ensure services are no installed before installing; resolves issues where re-provision occurs

--- .config/apollo-setup.sh
+++ .config/apollo-setup.sh
@@ -40,85 +40,136 @@ service postfix restart
 
 ## Install Nginx
 
-echo "Installing Nginx..."
+if ! which nginx > /dev/null 2>&1; then
 
-apt-get install nginx -y > /dev/null 2>&1
+	echo "Installing Nginx..."
 
-sed -i "s/worker_connections 768/worker_connections 1024/" /etc/nginx/nginx.conf
-sed -i "s/# multi_accept on/multi_accept on/" /etc/nginx/nginx.conf
-sed -i "s/keepalive_timeout 65/keepalive_timeout 15/" /etc/nginx/nginx.conf
-sed -i "s/# server_tokens off/server_tokens off; client_max_body_size 64m; fastcgi_buffers 16 16k; fastcgi_buffer_size 32k/" /etc/nginx/nginx.conf
+	apt-get install nginx -y > /dev/null 2>&1
 
-rm -R /etc/nginx/sites-available/*
-rm -R /etc/nginx/sites-enabled/*
-cp /vagrant/.config/000-default.conf /etc/nginx/sites-enabled/
-cp /vagrant/.config/index.html /projects/sites/000-default/
-service ngxinx restart > /dev/null 2>&1
+	sed -i "s/worker_connections 768/worker_connections 1024/" /etc/nginx/nginx.conf
+	sed -i "s/# multi_accept on/multi_accept on/" /etc/nginx/nginx.conf
+	sed -i "s/keepalive_timeout 65/keepalive_timeout 15/" /etc/nginx/nginx.conf
+	sed -i "s/# server_tokens off/server_tokens off; client_max_body_size 64m; fastcgi_buffers 16 16k; fastcgi_buffer_size 32k/" /etc/nginx/nginx.conf
 
+	rm -R /etc/nginx/sites-available/*
+	rm -R /etc/nginx/sites-enabled/*
+	cp /vagrant/.config/000-default.conf /etc/nginx/sites-enabled/
+	cp /vagrant/.config/index.html /projects/sites/000-default/
+	service ngxinx restart > /dev/null 2>&1
+
+else
+
+	echo "Nginx is already installed; skipping..."
+
+fi
 
 
 ## Install SSL certificate
 
 echo "Installing SSL certificate..."
 
-mkdir /etc/nginx/ssl
-openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout /etc/nginx/ssl/selfsigned.key -out /etc/nginx/ssl/selfsigned.crt -subj "/C=GB/ST=London/L=London/O=Apollo/OU=Apollo/CN=*.dev" > /dev/null 2>&1
+# Ensure the folder does not exist..
+if [ ! -d "/etc/nginx/ssl" ]; then
 
+	mkdir /etc/nginx/ssl
+	openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout /etc/nginx/ssl/selfsigned.key -out /etc/nginx/ssl/selfsigned.crt -subj "/C=GB/ST=London/L=London/O=Apollo/OU=Apollo/CN=*.dev" > /dev/null 2>&1
+
+fi
 
 ## Install MySQL
 
-echo "Installing MySQL Server..."
+if ! which mysql > /dev/null 2>&1; then
 
-debconf-set-selections <<< "mysql-server mysql-server/root_password password root"
-debconf-set-selections <<< "mysql-server mysql-server/root_password_again password root"
+	echo "Installing MySQL Server..."
 
-apt-get install mysql-server -y > /dev/null 2>&1
+	debconf-set-selections <<< "mysql-server mysql-server/root_password password root"
+	debconf-set-selections <<< "mysql-server mysql-server/root_password_again password root"
 
+	apt-get install mysql-server -y > /dev/null 2>&1
+
+else
+
+	echo "MySQL Server is already installed; skipping..."
+
+fi
 
 ## Install PHP 7
 
-echo "Installing PHP 7 and dependencies..."
+if ! which php7.0 > /dev/null 2>&1; then
 
-apt-get install php7.0-bcmath php7.0-cgi php7.0-cli php7.0-curl php7.0-dev php7.0-fpm php7.0-gd php7.0-mbstring php7.0-mcrypt php7.0-mysql php7.0-imap php7.0-json php7.0-pspell php7.0-soap php7.0-xml php7.0-xmlrpc php7.0-zip php-imagick php-memcache php-pear -y > /dev/null 2>&1
+	echo "Installing PHP 7 and dependencies..."
 
-sed -i "s/;cgi.fix_pathinfo=1/cgi.fix_pathinfo=0/" /etc/php/7.0/fpm/php.ini
-sed -i "s/upload_max_filesize = 2M/upload_max_filesize = 64M/" /etc/php/7.0/fpm/php.ini
-sed -i "s/post_max_size = 8M/post_max_size = 64M/" /etc/php/7.0/fpm/php.ini
+	apt-get install php7.0-bcmath php7.0-cgi php7.0-cli php7.0-curl php7.0-dev php7.0-fpm php7.0-gd php7.0-mbstring php7.0-mcrypt php7.0-mysql php7.0-imap php7.0-json php7.0-pspell php7.0-soap php7.0-xml php7.0-xmlrpc php7.0-zip php-imagick php-memcache php-pear -y > /dev/null 2>&1
+
+	sed -i "s/;cgi.fix_pathinfo=1/cgi.fix_pathinfo=0/" /etc/php/7.0/fpm/php.ini
+	sed -i "s/upload_max_filesize = 2M/upload_max_filesize = 64M/" /etc/php/7.0/fpm/php.ini
+	sed -i "s/post_max_size = 8M/post_max_size = 64M/" /etc/php/7.0/fpm/php.ini
+
+else
+
+	echo "PHP 7 and dependencies are already installed; skipping..."
+
+fi
 
 
 ## Install Mailcatcher
 
-echo "Installing Mailcatcher... (this takes a while)"
+if ! which mailcatcher > /dev/null 2>&1; then
 
-gem install mailcatcher --no-rdoc --no-ri > /dev/null 2>&1
+	echo "Installing Mailcatcher... (this takes a while)"
 
-echo "@reboot root $(which mailcatcher) --ip=0.0.0.0" >> /etc/crontab
-update-rc.d cron defaults
+	gem install mailcatcher --no-rdoc --no-ri > /dev/null 2>&1
 
-echo "sendmail_path = /usr/bin/env $(which catchmail) -f 'vagrant@apollo'" >> /etc/php/7.0/mods-available/mailcatcher.ini
+	echo "@reboot root $(which mailcatcher) --ip=0.0.0.0" >> /etc/crontab
+	update-rc.d cron defaults
 
-phpenmod mailcatcher
+	echo "sendmail_path = /usr/bin/env $(which catchmail) -f 'vagrant@apollo'" >> /etc/php/7.0/mods-available/mailcatcher.ini
 
-/usr/bin/env $(which mailcatcher) --ip=0.0.0.0 > /dev/null 2>&1
+	phpenmod mailcatcher
+
+	/usr/bin/env $(which mailcatcher) --ip=0.0.0.0 > /dev/null 2>&1
+
+else
+
+	echo "Mailcatcher is already installed; skipping..."
+
+fi
 
 
 ## Install WP-CLI
 
-echo "Installing WP-CLI..."
+if [ ! -f /usr/local/bin/wp ]; then
 
-cd /
-curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar > /dev/null 2>&1
-chmod +x wp-cli.phar
-mv wp-cli.phar /usr/local/bin/wp
+	echo "Installing WP-CLI..."
+
+	cd /
+	curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar > /dev/null 2>&1
+	chmod +x wp-cli.phar
+	mv wp-cli.phar /usr/local/bin/wp
+
+else
+
+	echo "WP CLI is already installed; skipping..."
+
+fi
 
 
 ## Install WordPress Template
 
-echo "Installing WordPress Template..."
 
-cd /projects/sites/000-template
-wp core download --allow-root > /dev/null 2>&1
-cp /vagrant/.config/wp-config.php .
+if [ ! -f /projects/sites/000-template/wp-load.php ]; then
+
+	echo "Installing WordPress Template..."
+
+	cd /projects/sites/000-template
+	wp core download --allow-root > /dev/null 2>&1
+	cp /vagrant/.config/wp-config.php .
+
+else
+
+	echo "WP Template is already installed; skipping..."
+
+fi
 
 
 ## Cleanup
