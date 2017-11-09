3b9006d34d3b84bf5c627aaffbd17be4fc7dc6a7
James Morrison <james@morrison.me>
2017-11-09 16:49:23 +0000

    1st pass at a better 404 page

---
+++ .config/index.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html>
+<head>
+
+	<meta charset="utf-8">
+
+	<title>Apollo</title>
+
+	<style>
+		body {
+			color: #333;
+			font-family: proxima-nova, sans-serif;
+			font-size: 16px;
+			font-weight: 400;
+			line-height: 1.25;
+			margin: 50px 25px;
+		}
+		
+		h1 {
+			font-weight: 300;
+			margin:0;
+			padding:0; 
+			font-size: 40px;
+			color: #242d37;
+		}
+		
+		hr {
+			background: #DFDFDF;
+			height: 1px;
+			border:0;
+			margin: 20px 0 30px;
+		}
+	</style>
+
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+
+</head>
+
+<body>
+
+	<h1>Apollo</h1>
+
+	<hr />
+
+	<p>Useful links:</p>
+	
+	<ul>
+		<li><a href="https://github.com/jamesmorrison/apollo/wiki">Apollo Wiki</a></li>
+		<li><a href="https://github.com/jamesmorrison/apollo/issues/new">Raise a bug / issue / ask a question</a></li>
+	</ul>
+	
+	<hr />
+	
+
+</body>
+</html>
