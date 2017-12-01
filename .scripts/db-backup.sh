c727948ff08be480598792c3a31992b7ed1c7fcc
James Morrison <james@morrison.me>
2017-12-01 15:59:27 +0000

    Switched DB Backup to on load to avoid the need for vagrant-triggers

--- .scripts/db-backup.sh
+++ .scripts/db-backup.sh
@@ -11,7 +11,9 @@ export MYSQL_PWD=$MYSQL_PASSWORD
 mkdir -p "$BACKUP_DIR"
 
 databases=`mysql --user=$MYSQL_USER -e "SHOW DATABASES;" | grep -Ev "(Database|information_schema|performance_schema|phpmyadmin|sys|mysql)"`
- 
+
+echo "Backing up your databases; this can take a few minutes..."
+
 for db in $databases; do
   mysqldump --force --opt --user=$MYSQL_USER --databases $db > "$BACKUP_DIR/$db.sql"
   echo "Database $db backed up..."
