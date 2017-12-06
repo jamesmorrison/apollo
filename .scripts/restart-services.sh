c63b0ff1f4e2b68dd7923b9f0121b3351548f74b
James Morrison <james@morrison.me>
2017-12-06 10:31:00 +0000

    Typo, services only tried to restart when they didn't exist..

--- .scripts/restart-services.sh
+++ .scripts/restart-services.sh
@@ -8,16 +8,16 @@ service mysql restart
 service nginx restart
 
 # Conditionally restart PHP 7.0
-if ! which php7.0 > /dev/null 2>&1; then
+if which php7.0 > /dev/null 2>&1; then
 	service php7.0-fpm restart
 fi
 
 # Conditionally restart PHP 7.1
-if ! which php7.1 > /dev/null 2>&1; then
+if which php7.1 > /dev/null 2>&1; then
 	service php7.1-fpm restart
 fi
 
 # Conditionally restart PHP 7.2
-if ! which php7.2 > /dev/null 2>&1; then
+if which php7.2 > /dev/null 2>&1; then
 	service php7.2-fpm restart
 fi
